3d54c10d6dfaee9ecf68fc5d487b64490c9dd56d
Rafida <rafidakn@gmail.com>
2023-05-21 18:38:50 +0200

    Initial Commit

---
+++ See Hear/See_HearApp.swift
@@ -0,0 +1,17 @@
+//
+//  See_HearApp.swift
+//  See Hear
+//
+//  Created by Rafida on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct See_HearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
